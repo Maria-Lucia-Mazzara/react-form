0e8efafcda4a298a51d9282725fc32927b246ea9
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-10 12:56:32 +0100

    aggiunta array di oggetti

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 
 
 function App() {
+  const libri = [
+    { id: 1, titolo: 'Il nome della rosa' },
+    { id: 2, titolo: 'Cent anni di solitudine' },
+    { id: 3, titolo: 'La coscienza di Zeno' },
+    { id: 4, titolo: '1984' },
+    { id: 5, titolo: 'Il giovane Holden' },
+  ];
   const [count, setCount] = useState(0)
 
   return (
